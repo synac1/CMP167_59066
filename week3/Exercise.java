d8377b5abfc005f38bde23ee2132cc872b9b52b5
Yanilda <synac1@users.noreply.github.com>
2021-03-03 09:36:03 -0500

    Exercise completed

--- week3/Exercise.java
+++ week3/Exercise.java
@@ -3,6 +3,32 @@
 //compare user's input to the original password and username
 //If the user enters the correct credentials print the correct message
 //else print access denied
-public class Exercise {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Exercise {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner keyboard = new Scanner( System.in );
+
+		File fileCred = new File("C:\\Users\\Yanil\\Desktop\\credentials.txt"); //accessing
+
+		Scanner sc = new Scanner(fileCred); // Reading 
+
+		String storedUser = sc.nextLine();
+		String storedPwd  = sc.nextLine();
+		//Input
+		System.out.println("Welcome to Login 360");
+		System.out.print("Enter Username: ");
+		String username = keyboard.next();
+		System.out.print("Enter Password: ");
+		String password = keyboard.next();
+
+		if(username.equals(storedUser) && password.equals(storedPwd)) 
+			
+			System.out.println("Access successful");
+
+		else {
+			System.out.println("Access Denied"); }
+		}
 }
