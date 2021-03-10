984429da436a77f38fb9716ce87aae334a3977bc
Yanilda <synac1@users.noreply.github.com>
2021-03-10 15:51:00 -0500

    Exercise

--- week6/src/Exercise.java
+++ week6/src/Exercise.java
@@ -1,4 +1,9 @@
-
+/*
+ * Write three methods,  
+ * 1-  A method to get 1 input from the user and return the input, empty paramerlist, return a int 
+ * 2- A method that takes an int value as argument and returns the product of that value times 100
+ * 3- Test the main method, and combine the methods to display the results
+ */
 public class Exercise {
 
 }
